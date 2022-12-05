b208da9ce4078f8cd74b9bb22a45ec2572054013
Dishant <deeshant29@gmail.com>
2022-12-05 13:35:53 +0530

    after selling medicine from worker section updation is done

--- worker/SellMedicine.java
+++ worker/SellMedicine.java
@@ -2,9 +2,9 @@ package worker;
 
 import java.io.File;
 import java.util.Scanner;
-
 import datatypefordata.DataType;
 import serialno.Length;
+import java.io.FileWriter;
 
 public class SellMedicine {
 	public void sellMed() {
@@ -31,9 +31,10 @@ public class SellMedicine {
 			Length k = new Length();
 			int e = k.len();
 			
-			String fw = "";
+			String fw = null;
 			
-			while(true) {
+			boolean dyw = true;
+			while(dyw) {
 				System.out.print("Enter medicine name : ");
 				String med = sc5.next();
 				med = med.toLowerCase();
@@ -52,9 +53,30 @@ public class SellMedicine {
 				else {
 					System.out.println("Enter quantity of medicine customer want : ");
 					int qt = sc5.nextInt();
-					fw = arr[ms].name + " " + arr[ms].cmp + " " + qt + " " + (qt*arr[ms].ppq) + " " + arr[ms].name + "\n";
+					fw = arr[ms].name + " " + arr[ms].cmp + " " + qt + " " + (qt*arr[ms].ppq) + " " + arr[ms].type + "\n";
+					arr[ms].qty -= qt;
+					
+					f.createNewFile();
+					FileWriter fwt = new FileWriter("E:\\Eclipse\\Medical_Management_System_1.0\\src\\Data\\medicines_data\\data.txt");
+					
+					for(int d=0;d<(e-1);d++) {
+						String wt = arr[d].srn + " " + arr[d].name + " " + arr[d].cmp + " " + arr[d].qty + " " + arr[d].ppq + " " + arr[d].type + "\n";
+						fwt.write(wt);
+					}
+					
+					System.out.println("Customer want another medicine(y/n) : ");
+					char dywm = sc.next().charAt(0);
+					
+					if(dywm == 'y' || dywm == 'Y') {
+						dyw = true;
+					}
+					else if(dywm == 'n' || dywm == 'N') {
+						dyw = false;
+						fw += "\n";
+					}
+					
+					fwt.close();
 				}
-				fw = fw + "\n";
 			}
 		}
 		catch(Exception e) {
